7a37375b608a26b27d20f29b2989a49c41bd8d05
vancouvergorilla <cwen283@gmail.com>
2021-01-07 17:04:23 -0600

    fetch from firebase

---
+++ firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase';
+
+import 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDlv_n99f3gvELdpLXQkxLtk1PYnckXnlA",
+  authDomain: "scheduler-94a34.firebaseapp.com",
+  databaseURL: "https://scheduler-94a34-default-rtdb.firebaseio.com",
+  projectId: "scheduler-94a34",
+  storageBucket: "scheduler-94a34.appspot.com",
+  messagingSenderId: "519686643176",
+  appId: "1:519686643176:web:de51c277c359033d31185d",
+  measurementId: "G-7W6BMDGKL1"
+
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export { firebase };
--- screens/ScheduleScreen.js
+++ screens/ScheduleScreen.js
@@ -3,10 +3,16 @@ import { SafeAreaView, StyleSheet, Text } from 'react-native';
 import CourseList from '../components/CourseList';
 import UserContext from '../contexts/UserContext';
 import CourseEditScreen from './CourseEditScreen';
+import {firebase} from '../firebase'
 
 const Banner = ({ title }) => (
   <Text style={styles.banner}>{title || '[loading...]'}</Text>
 );
+
+const fixCourses = json => ({
+  ...json,
+  courses: Object.values(json.courses)
+});
 
 const ScheduleScreen = ({navigation}) => {
   const [schedule, setSchedule] = useState({ title: '', courses: [] });
@@ -16,15 +22,13 @@ const ScheduleScreen = ({navigation}) => {
   const view = (course) => {
     navigation.navigate(canEdit ? 'CourseEditScreen' : 'CourseDetailScreen', { course });
   };
-  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
   useEffect(() => {
-    const fetchSchedule =  async () => {
-      const response = await fetch(url);
-      if (!response.ok) throw response;
-      const json = await response.json();
-      setSchedule(json);
+    const db = firebase.database().ref();
+    const handleData = snap => {
+      if (snap.val()) setSchedule(fixCourses(snap.val()));
     }
-    fetchSchedule();
+    db.on('value', handleData, error => alert(error));
+    return () => { db.off('value', handleData); };
   }, []);
 
   return (
